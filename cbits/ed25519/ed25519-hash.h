72d21a37b436abe8aae36db57b2e951b18bb300f
Emanuel Borsboom <manny@fpcomplete.com>
2019-03-12 12:12:22 -0700

    Use blake2b hash for ed25519 signing

--- cbits/ed25519/ed25519-hash.h
+++ cbits/ed25519/ed25519-hash.h
@@ -1,26 +1,26 @@
-#include <cryptonite_sha512.h>
-typedef struct sha512_ctx ed25519_hash_context;
+#include <cryptonite_blake2b.h>
+typedef blake2b_ctx ed25519_hash_context;
 
 static void
 ed25519_hash_init(ed25519_hash_context *ctx) {
-	cryptonite_sha512_init(ctx);
+	cryptonite_blake2b_init(ctx, 512);
 }
 
 static void
 ed25519_hash_update(ed25519_hash_context *ctx, const uint8_t *in, size_t inlen) {
-	cryptonite_sha512_update(ctx, in, inlen);
+	cryptonite_blake2b_update(ctx, in, inlen);
 }
 
 static void
 ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *hash) {
-	cryptonite_sha512_finalize(ctx, hash);
+	cryptonite_blake2b_finalize(ctx, 512, hash);
 }
 
 static void
 ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen) {
 	ed25519_hash_context ctx;
-	cryptonite_sha512_init(&ctx);
-	cryptonite_sha512_update(&ctx, in, inlen);
-	cryptonite_sha512_finalize(&ctx, hash);
+	cryptonite_blake2b_init(&ctx, 512);
+	cryptonite_blake2b_update(&ctx, in, inlen);
+	cryptonite_blake2b_finalize(&ctx, 512, hash);
 	memset(&ctx, 0, sizeof(ctx));
 }
